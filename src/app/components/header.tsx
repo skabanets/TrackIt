8e41dad97f0a0000b5e96f786fd91bb3a9d57222
Sergey Kabanets <skabanets97@gmail.com>
2024-08-30 21:46:24 +0300

    Add header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Logo, ThemeSwitcher } from "../components";
+
+export const Header = () => {
+  return (
+    <header className="bg-secondaryBgColor shadow-lg">
+      <div className="container flex items-center justify-between p-4">
+        <Logo />
+        <ThemeSwitcher />
+      </div>
+    </header>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import "modern-normalize/modern-normalize.css";
+import { Header } from "./components/header";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
