f7318d02407d710c62c47e3d8e262843bc6e35be
Sergey Kabanets <skabanets97@gmail.com>
2024-08-31 08:16:13 +0300

    Fix issue with status color

--- src/components/status-label.tsx
+++ src/components/status-label.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { getStatusColor } from "@/helpers";
 import { PurchaseStatus } from "@/types";
 
 interface StatusLabelProps {
   status: PurchaseStatus;
 }
 export const StatusLabel = ({ status }: StatusLabelProps) => {
+  const getStatusColor = (status: PurchaseStatus): string => {
+    switch (status) {
+      case "Process":
+        return "bg-processBgColor text-processTextColor";
+      case "Delivered":
+        return "bg-deliveredBgColor text-deliveredTextColor";
+      case "Cancelled":
+        return "bg-canceledBgColor text-canceledTextColor";
+      default:
+        return "";
+    }
+  };
+
   return (
     <div
       className={`w-fit rounded-[22px] px-3 py-2 text-xs leading-[12px] ${getStatusColor(status)}`}
