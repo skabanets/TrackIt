f9be053815a20e07909700993ea05d7c54bbb8ae
Sergey Kabanets <skabanets97@gmail.com>
2024-08-31 08:01:18 +0300

    Add icons for theme switcher

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -3,6 +3,8 @@
 import { Themes } from "@/constants";
 import { Button } from "@/components";
 import { useTheme } from "next-themes";
+import SunSvg from "public/sun.svg";
+import MoonSvg from "public/moon.svg";
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
@@ -13,8 +15,12 @@ export const ThemeSwitcher = () => {
   };
 
   return (
-    <Button onClick={handleToggleTheme} type="button" className="secondary-btn">
-      Switch to {theme === Themes.Light ? Themes.Dark : Themes.Light} theme
+    <Button onClick={handleToggleTheme} type="button" className="secondary-btn mr-4">
+      {theme === Themes.Light ? (
+        <MoonSvg className="h-5 w-5 fill-textColor" />
+      ) : (
+        <SunSvg className="h-5 w-5 fill-textColor" />
+      )}
     </Button>
   );
 };
