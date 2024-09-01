782f3f0dbdac5d3b0e43c1d72a17185d33d0a5a6
Sergey Kabanets <skabanets97@gmail.com>
2024-09-01 19:48:09 +0300

    Update limit for pages

--- src/components/items-per-page-selector.tsx
+++ src/components/items-per-page-selector.tsx
@@ -3,18 +3,23 @@
 import { itemsQuantityOptions } from "@/constants";
 import React, { useEffect, useRef, useState } from "react";
 import ArrowSvg from "public/arrow.svg";
+import { useSearchParams } from "next/navigation";
 
 export const ItemsPerPageSelector = () => {
+  const searchParams = useSearchParams();
   const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState<number | null>(null);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const [limit, setLimit] = useState<number | null>(null);
+  const options = itemsQuantityOptions;
 
   useEffect(() => {
-    const savedValue = localStorage.getItem("itemsPerPage");
-    if (savedValue) {
-      setValue(parseInt(savedValue, 10));
+    const limitFromParams = searchParams.get("limit");
+    if (limitFromParams) {
+      setLimit(Number(limitFromParams));
+    } else {
+      setLimit(10);
     }
-  }, []);
+  }, [searchParams]);
 
   useEffect(() => {
     const handleCloseList = (e: MouseEvent) => {
@@ -29,23 +34,23 @@ export const ItemsPerPageSelector = () => {
     };
   }, []);
 
-  useEffect(() => {
-    if (value !== null) {
-      localStorage.setItem("itemsPerPage", value.toString());
-    }
-  }, [value]);
-
   const handleDropdownClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
-    setIsOpen(!isOpen);
+    setIsOpen(prev => !prev);
   };
 
   const handleSelectOption = (option: number) => {
-    setValue(option);
-    setIsOpen(false);
-  };
+    if (typeof window !== "undefined") {
+      const searchParams = new URLSearchParams(window.location.search);
+      searchParams.set("limit", option.toString());
+      searchParams.set("page", "1"); // Reset page to 1
+      const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+      window.history.pushState({}, "", newUrl);
 
-  const options = itemsQuantityOptions;
+      setLimit(option);
+      setIsOpen(false);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center gap-3 text-xs">
@@ -57,7 +62,7 @@ export const ItemsPerPageSelector = () => {
           className={`px-[9px]rounded-lg flex h-[31px] w-[43px] cursor-pointer items-center gap-1 rounded-lg border bg-paginationBgColor px-2 py-2 text-textColor ${isOpen ? "border-searchBarColor" : "border-paginationBgColor"}`}
           onClick={handleDropdownClick}
         >
-          {value}{" "}
+          {limit}{" "}
           <ArrowSvg className={`h-2 w-2 shrink-0 fill-searchBarColor ${isOpen && "rotate-180"}`} />
         </div>
         {isOpen && (
@@ -67,7 +72,7 @@ export const ItemsPerPageSelector = () => {
                 key={option.id}
                 value={option.value}
                 className={`cursor-pointer px-2 first:rounded-t-[4px] last:rounded-b-[4px] hover:bg-accentColor ${
-                  value === option.value && "bg-sortArrowColor"
+                  limit === option.value && "bg-sortArrowColor"
                 }`}
                 onClick={() => handleSelectOption(option.value)}
               >
