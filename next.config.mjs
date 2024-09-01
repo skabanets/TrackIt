daeaeb2c242478155e493b967e1a880a2ba38fec
Sergey Kabanets <skabanets97@gmail.com>
2024-09-01 19:46:28 +0300

    Add api and data

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: ["hotline.ua"],
+    domains: ["www.istore.ua"],
   },
 };
 
---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import fs from "fs/promises";
+import { Order } from "@/types/order";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get("page") || "1";
+  const limit = searchParams.get("limit") || "10";
+  const search = searchParams.get("search") || "";
+
+  const filePath = path.join(process.cwd(), "src", "app", "api", "orders", "orders.json");
+
+  try {
+    const fileContents = await fs.readFile(filePath, "utf8");
+    const orders: Order[] = JSON.parse(fileContents);
+
+    const filteredOrders = orders.filter(order =>
+      order.productName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+    const startIndex = (pageNumber - 1) * limitNumber;
+    const paginatedOrders = filteredOrders.slice(startIndex, startIndex + limitNumber);
+
+    const totalPages = Math.ceil(filteredOrders.length / limitNumber);
+
+    return NextResponse.json({ orders: paginatedOrders, totalPages });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error reading or processing the JSON file" },
+      { status: 500 }
+    );
+  }
+}
